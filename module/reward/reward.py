4ae375632e28d1446eba2f9c1ee26a6a2c0adfd1
takagi <3197137842@qq.com>
2023-06-15 17:40:05 +0800

    fix: 领取特殊竞技场点数更新至 Blue Water Island

--- module/reward/reward.py
+++ module/reward/reward.py
@@ -6,6 +6,10 @@ from module.ui.page import *
 from module.ui.ui import UI
 
 
+class NoRewards(Exception):
+    pass
+
+
 class Reward(UI):
     def receive_reward(self, skip_first_screenshot=True):
         logger.hr('Reward receive')
@@ -72,7 +76,7 @@ class Reward(UI):
 
     def receive_special_arena_point(self, skip_first_screenshot=True):
         logger.hr('Special Arena Point receive')
-        confirm_timer = Timer(2, count=3).start()
+        confirm_timer = Timer(5, count=4).start()
         click_timer = Timer(0.3)
         while 1:
             if skip_first_screenshot:
@@ -81,19 +85,47 @@ class Reward(UI):
                 self.device.screenshot()
 
             if click_timer.reached() and self.appear_then_click(RECEIVE_SPECIAL_ARENA_POINT, offset=(30, 30),
-                                                                interval=1, static=False):
-                pass
-
-            if self.handle_reward(interval=1):
+                                                                interval=5, static=False):
                 confirm_timer.reset()
                 click_timer.reset()
                 continue
 
+            if self.appear(NO_REWARDS, offset=(5, 5), threshold=0.95, static=False):
+                raise NoRewards
+
+            if click_timer.reached() and self.appear_then_click(REWARD, offset=(30, 30),
+                                                                interval=5, static=False):
+                confirm_timer.reset()
+                click_timer.reset()
+                continue
+
+            if self.handle_reward(interval=1):
+                logger.info('Special Arena Point receive end')
+                raise NoRewards
+
             if confirm_timer.reached():
+                raise NoRewards
+
+        return True
+
+    def ensure_back(self, skip_first_screenshot=True):
+        click_timer = Timer(0.3)
+        while 1:
+            if skip_first_screenshot:
+                skip_first_screenshot = False
+            else:
+                self.device.screenshot()
+
+            if click_timer.reached() and self.handle_reward(interval=1):
+                click_timer.reset()
+                continue
+
+            if self.appear(SPECIAL_ARENA_CHECK, offset=(5, 5), static=False):
                 break
 
-        logger.info('Special Arena Point receive end')
-        return True
+            if click_timer.reached():
+                self.device.click_minitouch(100, 100)
+                click_timer.reset()
 
     def run(self):
         self.ui_ensure(page_reward)
@@ -103,5 +135,8 @@ class Reward(UI):
             self.receive_social_point()
         if self.config.Reward_CollectSpecialArenaPoint:
             self.ui_ensure(page_special_arena)
-            self.receive_special_arena_point()
+            try:
+                self.receive_special_arena_point()
+            except NoRewards:
+                self.ensure_back()
         self.config.task_delay(success=True)
